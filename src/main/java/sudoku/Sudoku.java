11b46886557c02491dc6310fed979147629f6fa1
Jay Harris <jaywritescode@users.noreply.github.com>
2021-04-04 19:47:52 -0400

    complete sudoku implementation

--- src/main/java/sudoku/Sudoku.java
+++ src/main/java/sudoku/Sudoku.java
@@ -2,8 +2,9 @@ package sudoku;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Range;
+import csp.ExactCoverProblem;
 
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,82 @@ public class Sudoku {
         this.boxesPerBand = boxesPerBand;
         this.boxesPerStack = boxesPerStack;
     }
+
+    public Set<Candidate> solve() {
+        return new ExactCoverProblem<>(candidates(), constraints()) {
+            @Override
+            public boolean relation(Constraint constraint, Candidate candidate) {
+                return constraint.isSatisfiedBy(candidate);
+            }
+        }.solve();
+    }
+
+    private Set<Constraint> constraints() {
+        Set<Constraint> constraints = new HashSet<>();
+        for (int row = 1; row <= size; ++row) {
+            for (int column = 1; column <= size; ++column) {
+                for (char digit : domain) {
+                    constraints.add(RowColumnConstraint.from(row, column));
+                    constraints.add(RowDigitConstraint.from(row, digit));
+                    constraints.add(ColumnDigitConstraint.from(column, digit));
+                    constraints.add(BoxDigitConstraint.from(mapToBox(row, column), digit));
+                }
+            }
+        }
+        return constraints;
+    };
+
+    private Set<Candidate> candidates() {
+        Set<Candidate> candidates = new HashSet<>();
+        for (var row = 1; row <= size; ++row) {
+            for (var column = 1; column <= size; ++column) {
+                Optional<Candidate> given = findGiven(row, column);
+
+                if (given.isPresent()) {
+                    candidates.add(given.get());
+                }
+                else {
+                    candidates.addAll(findCandidatesFor(row, column));
+                }
+            }
+        }
+        return candidates;
+    }
+
+    private Optional<Candidate> findGiven(int row, int column) {
+        return givens.stream()
+                .filter(candidate -> candidate.row == row && candidate.column == column)
+                .findFirst();
+    }
+
+    private Collection<Candidate> findCandidatesFor(int row, int column) {
+        return domain.stream()
+                .map(digit -> new Candidate(row, column, digit))
+                .filter(isValidWithGivens)
+                .collect(Collectors.toSet());
+    }
+
+    private Predicate<Candidate> isValidWithGivens = candidate -> List.of(
+            RowDigitConstraint.from(candidate.row, candidate.digit),
+            ColumnDigitConstraint.from(candidate.column, candidate.digit),
+            BoxDigitConstraint.from(mapToBox(candidate.row, candidate.column), candidate.digit))
+            .stream()
+            .noneMatch(this::hasGivenSatisfyingConstraint);
+
+
+    private boolean hasGivenSatisfyingConstraint(Constraint constraint) {
+        return givens.stream().anyMatch(constraint::isSatisfiedBy);
+    }
+
+    private Box mapToBox(int row, int column) {
+        int topLeftRow = (int) (row / boxesPerStack) + 1;
+        int topLeftColumn = (int) (column / boxesPerBand) + 1;
+
+        return Box.create(
+                Range.closed(topLeftRow, topLeftRow + boxesPerBand),
+                Range.closed(topLeftColumn, topLeftColumn * boxesPerStack));
+    }
+
 
     private static boolean validateInitialState(Set<Candidate> givens, Set<Character> domain, int size) {
         Range<Integer> range = Range.closed(1, size);
@@ -49,10 +126,7 @@ public class Sudoku {
                 "Expected size %s to be a perfect square.", size);
 
         Set<Character> domain = givens.stream().map(Candidate::getDigit).collect(Collectors.toSet());
-        Preconditions.checkArgument(domain.size() < size,
-                "Expected %s digits but only found %s. Cannot construct domain.",
-                size, domain.size());
-        Preconditions.checkArgument(domain.size() > size,
+        Preconditions.checkArgument(domain.size() == size,
                 "Expected %s digits but found %s. Cannot construct domain.",
                 size, domain.size());
 
