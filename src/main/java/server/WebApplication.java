3fa58f49376f572f451be0d6fb8dfc0e06befb8d
Jay Harris <jaywritescode@users.noreply.github.com>
2021-04-18 17:45:49 -0400

    add static resource path, post handler

--- src/main/java/server/WebApplication.java
+++ src/main/java/server/WebApplication.java
@@ -1,10 +1,21 @@
 package server;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.json.JavalinJackson;
 
 public class WebApplication {
     public static void main(String... args) {
-        Javalin app = Javalin.create().start(7000);
+        Javalin app = Javalin.create(config -> {
+            config.defaultContentType = "application/json";
+            config.addStaticFiles("/webapp/public");
+        }).start(7000);
+
         app.get("/solve", ctx -> ctx.result("hello world"));
+
+        app.post("/solve", ctx -> {
+            var puzzle = ctx.body();
+            System.err.println(puzzle);
+            ctx.status(204);
+        });
     }
 }
