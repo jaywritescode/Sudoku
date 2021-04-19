6c9c6f8f6541976dc3db95388d200ffd16e683c2
Jay Harris <jaywritescode@users.noreply.github.com>
2021-04-19 18:46:12 -0400

    submit request, no receiver on server side yet

--- src/main/resources/webapp/src/App.jsx
+++ src/main/resources/webapp/src/App.jsx
@@ -16,7 +16,7 @@ export default class App extends React.Component {
       puzzle: new Map(),
     };
 
-    _.bindAll(this, "onUpdateField", "onUpdateGrid", "onClear");
+    _.bindAll(this, "onUpdateField", "onUpdateGrid", "onClear", "onSubmit");
   }
 
   onUpdateField(evt) {
@@ -28,18 +28,43 @@ export default class App extends React.Component {
   }
 
   onUpdateGrid(row, column, digit = "") {
-    this.setState(produce(draft => {
-      if (digit.length) {
-        draft.puzzle.set(`${row}-${column}`, { row, column, digit, isGiven: true });
-      }
-      else {
-        draft.puzzle.delete(`${row}-${column}`);
-      }
-    }));
+    this.setState(
+      produce((draft) => {
+        if (digit.length) {
+          draft.puzzle.set(`${row}-${column}`, {
+            row,
+            column,
+            digit,
+            isGiven: true,
+          });
+        } else {
+          draft.puzzle.delete(`${row}-${column}`);
+        }
+      })
+    );
   }
 
   onClear() {
-    this.setState(produce(draft => draft.puzzle.clear()));
+    this.setState(produce((draft) => draft.puzzle.clear()));
+  }
+
+  async onSubmit() {
+    const { boxheight, boxwidth, puzzle } = this.state;
+    const givens = Array.from(puzzle.values()).map(({ row, column, digit }) =>
+      Object.assign({ row, column, digit })
+    );
+
+    let response = await window.fetch("http://localhost:7000/solve", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        boxHeight: boxheight,
+        boxWidth: boxwidth,
+        givens,
+      }),
+    });
   }
 
   renderDimensionBox(dimension) {
@@ -68,6 +93,10 @@ export default class App extends React.Component {
 
         <button name="reset" onClick={this.onClear}>
           reset
+        </button>
+
+        <button name="solve" onClick={this.onSubmit}>
+          solve
         </button>
 
         <Sudoku onUpdate={this.onUpdateGrid} {...this.state} />
