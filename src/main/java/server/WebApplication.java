dfc66d7a97848b70894b403c1341f8cdce66c20d
Jay Harris <jaywritescode@users.noreply.github.com>
2021-04-19 12:18:34 -0400

    deserialize with jackson. wip

--- src/main/java/server/WebApplication.java
+++ src/main/java/server/WebApplication.java
@@ -1,9 +1,25 @@
 package server;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.javalin.Javalin;
 import io.javalin.plugin.json.JavalinJackson;
 
+import java.util.stream.Collectors;
+
 public class WebApplication {
+
+    private static JsonNode perform(JsonNode puzzle) {
+        var boxHeight = puzzle.get("boxheight").asInt();
+        var boxWidth = puzzle.get("boxwidth").asInt();
+        var domain = puzzle.findValuesAsText("domain")
+                .stream()
+                .map(s -> s.charAt(0))
+                .collect(Collectors.toSet());
+        var candidates = puzzle.findValues("puzzle");
+
+        return null;
+    }
+
     public static void main(String... args) {
         Javalin app = Javalin.create(config -> {
             config.defaultContentType = "application/json";
@@ -13,7 +29,8 @@ public class WebApplication {
         app.get("/solve", ctx -> ctx.result("hello world"));
 
         app.post("/solve", ctx -> {
-            var puzzle = ctx.body();
+            var puzzle = JavalinJackson.defaultObjectMapper().readTree(ctx.body());
+            perform(puzzle);
             System.err.println(puzzle);
             ctx.status(204);
         });
